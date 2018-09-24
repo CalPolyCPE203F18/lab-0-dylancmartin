d41ef13ecf385b2688669dd71cb91ddafc507a60
dylancmartin <34138811+dylancmartin@users.noreply.github.com>
2018-09-24 12:50:14 -0700

    working
    working

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,24 @@
 public class Lab00
 {
-   public static void main(String[] args)
-   {
+    private int x = 3
+    private String = "hello"
+    private double = 9.8
 
-   
-   }
+    public static void main(String[] args)
+    {
+        System.out.println("x: " + x + "y: " + y + "z: " + z);
+        nums[] = [3,6,-1,2]
+        for (int num : nums)
+            System.out.println(num);
+        System.out.println();
+        int numFound = char_count(y,'1');
+        System.out.println("found:" + numFound);
+
+    }
+
+    public static int char_count(String s, char c)
+    {
+        i = 0
+        for (j)
+    }
 }
