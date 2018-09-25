24fc799e62091bfef71aa163f1ebb8d400069e73
dylancmartin <34138811+dylancmartin@users.noreply.github.com>
2018-09-24 20:54:31 -0700

    done
    done

--- Lab00.java
+++ Lab00.java
@@ -1,20 +1,47 @@
+import java.util.*;
+
+
 public class scratch
 {
-    private int x = 3;
-    private String y = "hello";
-    private double z = 9.8;
+    // declaring and initializing some variables
+
+    private static int x = 3;
+    private static String y = "hello";
+    private static double z = 9.8;
 
     public static void main(String[] args)
     {
-        System.out.println("x: " + x + "y: " + y + "z: " + z);
-        nums[int] = [3,6,-1,2];
+        // printing the variables
+
+        System.out.println("x: " + Integer.toString(x) +" " + "y: " + get_y() + " "+ "z: " + String.valueOf(z));
+        // a list (make an array in java)
+        int nums[] = {3,6,-1,2};
         for (int num : nums)
             System.out.println(num);
         System.out.println();
-        int numFound = char_count(y,'1');
+        //call a function
+        int numFound = char_count(y,'l');
         System.out.println("found:" + numFound);
-
+        for(int i = 1; i <= 10; i++)
+            System.out.println(i);
+        System.out.println();
     }
-
-}
+    public static int char_count(String s, char c)
+    {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c)
+                count += 1;
+        }
+        return count;
+    }
+    public static int get_x() {
+        return x;
+    }
+    public static String get_y() {
+        return y;
+    }
+    public static double get_z() {
+        return z;
+    }
 }
